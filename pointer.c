758e7969b7331bf95dce35138597d6ed905466a1
Sumit Maurya <sumitmaurya746@gmail.com>
2026-03-30 21:34:15 +0530

    pointers example

--- pointer.c
+++ pointer.c
@@ -3,8 +3,9 @@
 int main()
 {
     int a = 6;
-    int *ptr = &a;
-    printf("Hey C language \n"); 
-    printf("The value of a is %d", a);
+    int* ptr = &a; // pointer
+    printf("The value of a is %d \n", a); // direct printing
+    printf("The value of a is %d \n", *ptr); //*ptr conatins value at memory address &a
+    printf("The value of a is %d", ptr); // ptr contains memory address of a 
     return 0;
 }

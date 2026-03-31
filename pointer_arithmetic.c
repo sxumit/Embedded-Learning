d4374b0f632c52743d9058a850fbe1f98f0c4b63
Sumit Maurya <sumitmaurya746@gmail.com>
2026-03-31 21:49:55 +0530

    pointer arithmetic addition

--- pointer_arithmetic.c
+++ pointer_arithmetic.c
@@ -5,10 +5,13 @@ int main(){
     //assume that address of 10 is 1000
     //address of 20 is 1004 and so on because int takes 4 byte space
 
-    printf("%d \n", *ptr); // prints the first number
+    printf("%d \n", *ptr); // prints the first number 10
     printf("%d \n", ptr); // prints the address of 10
 
     printf("%d \n",*(ptr+1)); // here +1 means 4 byte , so 1000+4 = 1004. means address of 20 
     printf("%d \n", (ptr+1)); // prints the address of 20
+
+    printf("%d \n",*(ptr+2)); // prints the number at 1000+8 , ie, 30
+    printf("%d \n", (ptr+2));
     return 0;
 }

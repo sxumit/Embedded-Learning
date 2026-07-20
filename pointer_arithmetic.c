06d17d5a1764cd349ff0f0a257a8c3d3f69cd200
Sumit Maurya <sumitmaurya746@gmail.com>
2026-07-20 10:55:42 +0530

    arithmetics updated

--- pointer_arithmetic.c
+++ pointer_arithmetic.c
@@ -3,7 +3,7 @@ int main(){
     int arr[] = {10,20,30,40,50};
     int *ptr = arr; // here we can use arr, &arr or arr[0]..by default the pointer take array in address form
     //assume that address of 10 is 1000
-    //address of 20 is 1004 and so on because int takes 4 byte space
+    //address of 20 is 1004 and so on because int takes 4 byte spacexs x  x vxvvv
 
     printf("Arithmetic Addition starts here- \n");
     printf("%d \n", *ptr); // prints the first number 10
